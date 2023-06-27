c3f4a459ce524d8cb46689f63a0fcda8b9e3f99c
sammo1235 <samrhysperry93@hotmail.co.uk>
2023-06-27 08:36:34 +0100

    add tests for mortgage schedule

--- src/mortgage_schedule/mod.rs
+++ src/mortgage_schedule/mod.rs
@@ -38,8 +38,13 @@ impl MortgageSchedule {
           total_interest_paid += interest_paid;
           // principal paid this month
           let mut principal_paid = minimum_payment_amount - interest_paid;
+          // add overpayment
           if self.overpayment_amount != 0.0 {
               principal_paid += self.overpayment_amount
+          }
+
+          if principal_paid > self.running_principal {
+            principal_paid = self.running_principal
           }
           
           // we pay off the principal
@@ -50,13 +55,89 @@ impl MortgageSchedule {
               total_repayment_cost += self.overpayment_amount;
           }
 
-          if self.running_principal.round() <= 0.0 {
+          if self.running_principal <= 0.0 {
               println!("Mortgage paid in {} years, {} months", payment_count / 12, payment_count % 12);
               println!("Total Interest Paid: £{}", round(total_interest_paid, 2));
               println!("Total Amount Paid: £{}", round(total_repayment_cost, 2));
               break;
           }
       }
-      Ok((total_repayment_cost, total_interest_paid))
+      Ok((round(total_repayment_cost, 2), round(total_interest_paid, 2)))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::mortgage_schedule::MortgageSchedule;
+
+  #[test]
+  fn small_loan_with_annual_rate() {
+    let Ok((total_cost, total_interest)) = MortgageSchedule {
+      principal: 10000.0, // £10k value
+      overpayment_amount: 0.0,
+      running_principal: 10000.0,
+      monthly_interest_rate: 0.02, // 2% interest annual rate
+      number_of_payments: 5, // 5 years
+    }.run_schedule() else { return; };
+
+    assert_eq!(total_cost, 10607.92);
+    assert_eq!(total_interest, 607.92)
+
+  }
+  
+  #[test]
+  fn calculates_mortgage_without_overpaying() {
+    let Ok((total_cost, total_interest)) = MortgageSchedule {
+      principal: 100000.0, // £100k value
+      overpayment_amount: 0.0,
+      running_principal: 100000.0,
+      monthly_interest_rate: 0.02/12.0, // 2% interest monthly rate
+      number_of_payments: 35*12, // 35 years
+    }.run_schedule() else { return; };
+
+    assert_eq!(total_cost, 139130.36);
+    assert_eq!(total_interest, 39130.36)
+  }
+
+  #[test]
+  fn calculates_mortgage_with_overpaying() {
+    let Ok((total_cost, total_interest)) = MortgageSchedule {
+      principal: 100000.0, // £100k value
+      overpayment_amount: 100.0, // £100 overpayment per month
+      running_principal: 100000.0,
+      monthly_interest_rate: 0.02/12.0, // 2% interest monthly rate
+      number_of_payments: 35*12, // 35 years
+    }.run_schedule() else { return; };
+
+    assert_eq!(total_cost, 126791.25);
+    assert_eq!(total_interest, 26511.89);
+  }
+
+  #[test]
+  fn calculates_mortgage_without_overpaying_2() {
+    let Ok((total_cost, total_interest)) = MortgageSchedule {
+      principal: 300000.0, // £300k value
+      overpayment_amount: 0.0,
+      running_principal: 300000.0,
+      monthly_interest_rate: 0.04/12.0, // 4% interest monthly rate
+      number_of_payments: 30*12, // 30 years
+    }.run_schedule() else { return; };
+
+    assert_eq!(total_cost, 515608.52);
+    assert_eq!(total_interest, 215608.52);
+  }
+
+  #[test]
+  fn calculates_mortgage_with_overpaying_2() {
+    let Ok((total_cost, total_interest)) = MortgageSchedule {
+      principal: 300000.0, // £300k value
+      overpayment_amount: 100.0,
+      running_principal: 300000.0,
+      monthly_interest_rate: 0.04/12.0, // 4% interest monthly rate
+      number_of_payments: 30*12, // 30 years
+    }.run_schedule() else { return; };
+
+    assert_eq!(total_cost, 487254.19);
+    assert_eq!(total_interest, 186861.93);
   }
 }
